356248fe3a7d65543836fb0db071f6507f593927
GaryCave <technicalhimanshu34@gmail.com>
2025-03-10 15:23:45 +0000

    conv basics

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "himanshu"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "bsdk"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "kuch bhi likh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
